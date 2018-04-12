05632c6294629d018a36c734c0b66a7e1b1f4fbb
Impetus <GrEedWish_X@outlook.com>
2018-04-12 11:39:17 -0600

    Add files via upload

--- heliosServlet/WEB-INF/src/helios/javadriver/Javamongo.java
+++ heliosServlet/WEB-INF/src/helios/javadriver/Javamongo.java
@@ -1,16 +1,11 @@
 package helios.javadriver;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 
@@ -30,28 +25,38 @@ import javax.json.JsonValue;
 import javax.json.JsonString;
 
 public class Javamongo{
-	private final static String databaseName = "Helios_Test";
+	private final static String databaseName = "Helios";
 
-	private final static String collectionName = "Server_Test";
+	private final static String histCollectionName = "TrafficData";
+
+	private final static String predCollectionName = "PredictionData";
 
 	private final static String hostPort = "localhost:27017";
 	
 	private MongoCollection<Document> collection;
 
 	private MongoClient mongoClient;
-	
-	public Javamongo(){
-		this(hostPort, databaseName, collectionName);	
-	}
+
+	public Javamongo(){}
 	
 	/**
 	 * Initialization, establish connection to MongoDB and open a specific collection
-	 * @param  hostPort host name and port number with format host:port
-	 * @param  dbName   database name in MongoDB
-	 * @param  colName  collection name in MongoDB database
+	 * @param  spec (JsonObject) specifications 
+	 *              - (String) hostPort: 	default local host:27017
+	 *              - (String) dbName:		default Helios database
+	 *              - (String) type:		default historical
 	 * @return
 	 */
-	public Javamongo(String hostPort, String dbName, String colName){
+	public void setup(JsonObject spec){
+		String hostPort = spec.getString("hostPort", this.hostPort);
+		String dbName = spec.getString("dbName", this.databaseName);
+		String type = spec.getString("type", "historical");
+		String colName;
+		if (type.equals("historical"))
+			colName = this.histCollectionName;
+		else
+			colName = this.predCollectionName;
+
 		MongoClientURI connectionString = new MongoClientURI("mongodb://" + hostPort);
 		mongoClient = new MongoClient(connectionString);
 		MongoDatabase database = mongoClient.getDatabase(dbName);
@@ -63,15 +68,18 @@ public class Javamongo{
 	 * query the database based on the specifications in Json format
 	 * 
 	 * @param  spec (JsonObject) specifications 
-	 * 			- (String) geohash: central geohash for querying, should less than 12 characters
+	 * 			- (String) geohash: a list of geohash box for querying, should less than 12 characters
 	 * 			- (String) timestamp: restrict data starting from a timestamp to present (TODO, from --> to)
 	 * 				+ 'from': A string of UTC time in milliseconds
 	 * 				+ 'to': A string of UTC time in milliseconds, could be None, if None, set to current time
 	 * 			- (String) zoomLevel: 
-	 * 				+ 'heatPoints': retrieve number of accidents happened in a specific geohash area
-	 * 				+ 'allData': retrieve detailed information of accidents happened in a specific geohash area
+	 * 				+ 'number': retrieve number of incidents happened in a specific geohash area
+	 * 				+ 'heatPoints': retrieve number of incidents happened in a specific geohash area
+	 * 				+ 'allData': retrieve detailed information of incidents happened in a specific geohash area
 	 * 			- (String) numLimit: total number of records return to client for a single query, default max int
-	 * 			- TODO: (String) type: historical or prediction
+	 * 			- (String) type: 
+	 * 				+ 'historical'
+	 * 				+ 'prediction'
 	 * @return     JsonArray with all records in JsonObject format
 	 */
 	public JsonArray queryCollectionRetJson(JsonObject spec){
@@ -88,8 +96,6 @@ public class Javamongo{
 				cursor = collection.find(filterBson).limit(numLimit).iterator();
 				break;
 			case "number":
-				// TODO Need improvment
-				// Cursor.count() does not work now
 				cursor = collection.find(filterBson).limit(numLimit).iterator();
 				int count  = 0;
 				while(cursor.hasNext()) {
@@ -114,7 +120,7 @@ public class Javamongo{
 	
 	/**
 	 * @param  spec (JsonObject) specifications 
-	 * 			- (String) geohash: ex. 9xh2
+	 * 			- (String) geohash: ex. ['9xh2', '9xh1']
 	 * 				+ central geohash for querying, should less than 12 characters
 	 * 			- (String) timestamp: 
 	 * 				+ 'from': A string of UTC time in milliseconds ex. 1522183300000
@@ -150,14 +156,18 @@ public class Javamongo{
 		
 		JsonObject jobj = Json.createObjectBuilder()
 							.add("geohash", Json.createArrayBuilder()
-												.add("9xh1")
+												.add("dr5")
 												.add("9xh2"))
 							.add("timestamp", Json.createObjectBuilder()
 												.add("from", "1522118500000")
 								)
 							.add("zoomLevel", "heatPoints")
+							.add("type", "historical")
 							.add("numLimit", "100").build();
+		
 		System.out.println(jobj);
+
+		javamongo.setup(jobj);
 		JsonArray jarr = javamongo.queryCollectionRetJson(jobj);
 		System.out.println(jarr.size());
 		for(JsonValue value : jarr){
